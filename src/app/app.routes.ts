586dfa27a503e20aa588dbfcee785f381a838337
Ibrahim Zaher <hemazaher15@gmail.com>
2025-08-16 10:31:51 -0800

    add layout and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,84 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
+import { BlankLayoutComponent } from './core/layouts/blank-layout/blank-layout.component';
+import { LoginComponent } from './core/auth/login/login.component';
+import { RegisterComponent } from './core/auth/register/register.component';
+import { HomeComponent } from './features/home/home.component';
+import { BrandsComponent } from './features/brands/brands.component';
+import { CategoriesComponent } from './features/categories/categories.component';
+import { DetailsComponent } from './features/details/details.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
+import { CartComponent } from './features/cart/cart.component';
+import { ProductsComponent } from './features/products/products.component';
+import path from 'path';
+import { NotfoundComponent } from './features/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+    ],
+  },
+  {
+    path: '',
+    component: BlankLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'brands',
+        component: BrandsComponent,
+      },
+      {
+        path: 'categories',
+        component: CategoriesComponent,
+      },
+      {
+        path: 'details',
+        component: DetailsComponent,
+      },
+      {
+        path: 'checkout',
+        component: CheckoutComponent,
+      },
+      {
+        path: 'cart',
+        component: CartComponent,
+      },
+      {
+        path: 'products',
+        component: ProductsComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: NotfoundComponent,
+  },
+];
